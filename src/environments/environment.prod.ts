e64c6d09c9c824e548aca94c0d80c07f35a30fb3
Mauricio Ampuero <mdampuero@gmail.com>
2024-04-20 20:03:12 +0200

    SSL compile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: true,
 	// baseBEUrl: 'https://clickgroup-backend-production.up.railway.app',
-	baseBEUrl: 'http://54.233.128.235:3000',
+	baseBEUrl: 'https://clickgroup-be.latamhosting.com.ar',
 	mapBox: {
 		apiKey: 'pk.eyJ1IjoibWRhbXB1ZXJvIiwiYSI6ImNsaDlwaW1tdDA5cW0zcnRkYWJxNW00NmMifQ.-UjTToqIopVTq7plocSHhQ',
 		defaultLng: -70.64827, 
